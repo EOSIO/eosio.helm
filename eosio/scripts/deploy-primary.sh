5dec0acdf729a9b6172f6d4982fc1480d7e1e289
William Blevins <william.blevins@block.one>
2021-02-22 16:42:19 -0500

    Better encapsulation for test scripts plus fix relative path issues.

--- eosio/scripts/deploy-primary.sh
+++ eosio/scripts/deploy-primary.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -eo pipefail
 echo '--- :evergreen_tree: Configuring Environment'
-./scripts/script.sh '-f primary.yaml' primary
+cd $(dirname "$0")/..
+scripts/script.sh '-f primary.yaml' primary
 echo 'Done.'
