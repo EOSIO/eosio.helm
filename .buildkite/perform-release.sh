34fb2b2803032fcec81caf329e5f34a494b73c84
William Blevins <william.blevins@block.one>
2021-02-25 19:11:13 -0500

    Better release script output.

--- .buildkite/perform-release.sh
+++ .buildkite/perform-release.sh
@@ -1,31 +1,57 @@
 #!/bin/bash
-set -xeo pipefail
+set -eo pipefail
 echo '+++ :helm: Executing Helm package release.'
 
-test -n "$EOSIO_HELM_CHART_REPO_BUCKET"
+command="test -n \"$EOSIO_HELM_CHART_REPO_BUCKET\""
+echo $command
+eval $command
 
-./eosio/scripts/helm-dependency-update.sh
+echo ""
+echo "- Updating subcharts."
+command="./eosio/scripts/helm-dependency-update.sh"
+echo $command
+eval $command
 
+echo ""
+echo "- Packaging charts."
 for directory in `ls -d eosio*`; do
-  # TODO - sign these packages
-  helm package $directory
+  # TODO sign these packages
+  command="helm package $directory"
+  echo $command
+  eval $command
 done
 
 original_index="index.yaml.orig"
 index="index.yaml"
 
-aws s3 cp s3://$EOSIO_HELM_CHART_REPO_BUCKET/$index $original_index
+echo ""
+echo "- Evaluating existing repo index."
+command="aws s3 cp s3://$EOSIO_HELM_CHART_REPO_BUCKET/$index $original_index"
+echo $command
+eval $command
 
 for package in `ls -f *.tgz`; do
   if [[ -z `cat $original_index | grep $package` ]]; then
-    aws s3 cp $package s3://$EOSIO_HELM_CHART_REPO_BUCKET
+    echo "Uploading package to repo."
+    command="aws s3 cp $package s3://$EOSIO_HELM_CHART_REPO_BUCKET"
+    echo $command
+    eval $command
   else
-    rm $package
+    echo "Not indexing package that already exists in the repo."
+    command="rm $package"
+    echo $command
+    eval $command
   fi
 done
 
-helm repo index --merge $original_index .
+echo ""
+echo "- Building updated index."
+command="helm repo index --merge $original_index ."
+echo $command
+eval $command
 
-aws s3 cp $index s3://$EOSIO_HELM_CHART_REPO_BUCKET/$index
+command="aws s3 cp $index s3://$EOSIO_HELM_CHART_REPO_BUCKET/$index"
+echo $command
+eval $command
 
 echo '+++ :white_check_mark: Done! Good luck :)'
